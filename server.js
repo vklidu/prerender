fb3a882590d6a6b78d0417ae49e49ff09bfabbb8
Jiří Majer <jiri.majer@artin.cz>
2015-10-02 10:54:33 +0200

    access log

--- server.js
+++ server.js
@@ -5,17 +5,31 @@ var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT,
+    accessLog: {
+      // Check out the file-stream-rotator docs for parameters
+      fileStreamRotator: {
+         filename: '/var/log/prerender/access-%DATE%.log',
+         frequency: 'daily',
+         date_format: 'YYYY-MM-DD',
+         verbose: true
+      },
+      // Check out the morgan docs for the available formats
+      morgan: {
+        format: 'combined'
+      }
+    }
 });
 
 
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
 server.use(prerender.blacklist());
-// server.use(prerender.logger());
+server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
-
+server.use(require('prerender-access-log'));
 server.start();
+
