a37e946931e56d4fa9fbbb76e6c23ce75159a15a
Jiří Majer <jiri.majer@artin.cz>
2015-10-02 10:58:45 +0200

    access log

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ server.use(prerender.blacklist());
 server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-// server.use(prerender.inMemoryHtmlCache());
+server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
 server.use(require('prerender-access-log'));
 server.start();
